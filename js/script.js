a2c60304e81db322098b9dd6686a2c236c4de8fd
Azumi-Thadz <111357004+Azumi-Thadz@users.noreply.github.com>
2022-09-06 15:45:42 +0800

    part1
    all done for part1

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// typing animation
+var typed = new Typed(".typing",{
+  strings:["","Front-end Web Developer","Web Designer","Ui/Ux Designer","Data-Analyst-Engineer"],
+  typeSpeed:100,
+  BackSpeed:60,
+  loop:true
+})
