cf5ce5cda6d9217cea8cdab554a4a9d22e3f2360
Azumi-Thadz <111357004+Azumi-Thadz@users.noreply.github.com>
2022-09-06 12:28:42 +0800

    Delete style-switcher.js

--- js/style-switcher.js
+++
@@ -1,5 +0,0 @@
-/* ==========toggle style Switcher=======*/
-const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
-styleSwitcherToggle.addEventListener("click", ()=> {
-  document.querySelector(".style-switcher").classList.toggle("open");
-})
