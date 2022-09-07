f0aa18b232e72a4ddc94161424f7d7affd976643
Azumi-Thadz <111357004+Azumi-Thadz@users.noreply.github.com>
2022-09-07 14:33:15 +0800

    final
    have trouble iwth js but its still ok

--- js/script.js
+++ js/script.js
@@ -25,6 +25,10 @@ const nav = document.querySelector(".nav"),
       }
       this.classList.add("active")
       showSection(this);
+      if(window.innerWidth <1200)
+      {
+        asideSectionTogglerBtn();
+      }
     })
   }
   function showSection(element)
@@ -36,3 +40,32 @@ const nav = document.querySelector(".nav"),
     const target = element.getAttribute("href").split("#")[1];
     document.querySelector("#" + target).classList.add("active")
   }
+  function updateNav(element)
+  {
+    for(let i=0; i<totalNavList; i++)
+    {
+      navList[i].querySelector("a").classList.remove("active");
+      const target = element.getAttribute("href").split("#")[1];
+    }
+  }
+  document.querySelector(".hire-me").addEventListener("click", function()
+  {
+  showSection(this);
+  updateNav(this);
+  })
+
+  const navTogglerBtn = document.querySelector(".nav-toggler"),
+    aside = document.querySelector(".aside");
+    navTogglerBtn.addEventListener("click", () => 
+    {
+      asideSectionTogglerBtn();
+    })
+    function asideSectionTogglerBtn()
+    {
+      aside.classList.toggle("open");
+      navTogglerBtn.classList.toggle("open");
+      for(let i=0; i<totalSection; i++)
+      {
+        allSection[i].classList.toggle("open");
+      }
+    }
